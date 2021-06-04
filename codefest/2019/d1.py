feee1481f92c3d5549767e268841968720f44995
Cem Sağlam <68742964+cemsaglam07@users.noreply.github.com>
2021-06-04 17:21:02 +0300

    Problem D, task 1

---
+++ codefest/2019/d1.py
@@ -0,0 +1,20 @@
+import re
+
+
+def ksy(x):
+    return re.sub(r'K[S]*Y', "S", x)
+
+
+control = []
+no = input()
+s = input().strip()
+while True:
+    s = ksy(s)
+    control.append(s)
+    if len(control) >= 2 and control[-2] == control[-1]:
+        break
+
+if control[-1][-1] == "S":
+    print("EVET")
+else:
+    print("HAYIR")
